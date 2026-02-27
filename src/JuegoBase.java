158fbf4581bcfcf330b59c79848b9b28e5870f56
Tyrion1078 <javihinjo@gamil.com>
2026-02-27 11:08:01 +0100

    Aquie lo que he hecho es que como me e dado cuenta que no habia puesto un archivo aparte los pokemons y que iva ha hacer lo mismo con los ataques e separado todos los pokemons en un archivo aparte llamado ListaPokemons y e creado otro llamado ListaAtaques para guardar ahora los ataques que cada pokemon pueda utilizar

--- src/JuegoBase.java
+++ src/JuegoBase.java
@@ -1,22 +1,16 @@
 import java.util.Scanner;
 public class JuegoBase {
-    public static void main(String[] args){
-     //crear pokemons
-    Pokemon pokemon1 =new Pokemon("garchomp", "tierra", "dragon", 239);
-    Pokemon pokemon2 =new Pokemon("Lycanroc", "roca","", 198);
-    Pokemon pokemon3 =new Pokemon("Golisopod", "bicho","agua", 181);
-    Pokemon pokemon4 =new Pokemon("Raichu_alola", "electrico","psiquico", 155);
-    Pokemon pokemon5 =new Pokemon("Kommo-o", "dragon","lucha", 181);
-    Pokemon pokemon6 =new Pokemon("Lucario", "lucha","acero", 172);
+    public static void main(String[] args){  
+        ListaPokemons pokedex = new ListaPokemons();
      //interfaz del juego
     System.out.println("=====================\nBienvenido\n=====================");
     System.out.println("Estos son los pokemons disponibles");
-    System.out.println("(1)"+ pokemon1.getName() +" tipo " + pokemon1.getType1() + " " + pokemon1.getType2());
-    System.out.println("(2)"+ pokemon2.getName() +" tipo " + pokemon2.getType1() + " " + pokemon2.getType2());
-    System.out.println("(3)"+ pokemon3.getName() +" tipo " + pokemon3.getType1() + " " + pokemon3.getType2());
-    System.out.println("(4)"+ pokemon4.getName() +" tipo " + pokemon4.getType1() + " " + pokemon4.getType2());
-    System.out.println("(5)"+ pokemon5.getName() +" tipo " + pokemon5.getType1() + " " + pokemon5.getType2());
-    System.out.println("(6)"+ pokemon6.getName() +" tipo " + pokemon6.getType1() + " " + pokemon6.getType2());
+    System.out.println("(1)"+ pokedex.getCatalogo()[0].getName() +" tipo " + pokedex.getCatalogo()[0].getType1() + " " + pokedex.getCatalogo()[0].getType2());
+    System.out.println("(2)"+ pokedex.getCatalogo()[1].getName() +" tipo " + pokedex.getCatalogo()[1].getType1() + " " + pokedex.getCatalogo()[1].getType2());
+    System.out.println("(3)"+ pokedex.getCatalogo()[2].getName() +" tipo " + pokedex.getCatalogo()[2].getType1() + " " + pokedex.getCatalogo()[2].getType2());
+    System.out.println("(4)"+ pokedex.getCatalogo()[3].getName() +" tipo " + pokedex.getCatalogo()[3].getType1() + " " + pokedex.getCatalogo()[3].getType2());
+    System.out.println("(5)"+ pokedex.getCatalogo()[4].getName() +" tipo " + pokedex.getCatalogo()[4].getType1() + " " + pokedex.getCatalogo()[4].getType2());
+    System.out.println("(6)"+ pokedex.getCatalogo()[5].getName() +" tipo " + pokedex.getCatalogo()[5].getType1() + " " + pokedex.getCatalogo()[5].getType2());
     System.out.println("eliga 3 pokemons para pelear");
     //pedimos que introduzca los numero de uno en uno de los 3 pokemons que quiere utilizar
     Scanner eleguirPokemons = new Scanner(System.in);
@@ -32,45 +26,45 @@ public class JuegoBase {
     String nomPokemon3="";
     switch (nuPokemon1) {
         case 1:
-            nomPokemon1=pokemon1.getName();
+            nomPokemon1=pokedex.getCatalogo()[0].getName();
             break;
-             case 2:
-            nomPokemon1=pokemon2.getName();
+        case 2:
+            nomPokemon1=pokedex.getCatalogo()[1].getName();
             break;
-             case 3:
-            nomPokemon1=pokemon3.getName();
+        case 3:
+            nomPokemon1=pokedex.getCatalogo()[2].getName();
             break;
-             case 4:
-            nomPokemon1=pokemon4.getName();
+        case 4:
+            nomPokemon1=pokedex.getCatalogo()[3].getName();
             break;
-             case 5:
-            nomPokemon1=pokemon5.getName();
+        case 5:
+            nomPokemon1=pokedex.getCatalogo()[4].getName();
             break;
-             case 6:
-            nomPokemon1=pokemon6.getName();
+        case 6:
+            nomPokemon1=pokedex.getCatalogo()[5].getName();
             break;
-   
+
         default:
             break;
     }
     switch (nuPokemon2) {
         case 1:
-            nomPokemon2=pokemon1.getName();
+            nomPokemon2=pokedex.getCatalogo()[0].getName();
             break;
-             case 2:
-            nomPokemon2=pokemon2.getName();
+        case 2:
+            nomPokemon2=pokedex.getCatalogo()[1].getName();
             break;
-             case 3:
-            nomPokemon2=pokemon3.getName();
+        case 3:
+            nomPokemon2=pokedex.getCatalogo()[2].getName();
             break;
-             case 4:
-            nomPokemon2=pokemon4.getName();
+        case 4:
+            nomPokemon2=pokedex.getCatalogo()[3].getName();
             break;
-             case 5:
-            nomPokemon2=pokemon5.getName();
+        case 5:
+            nomPokemon2=pokedex.getCatalogo()[4].getName();
             break;
-             case 6:
-            nomPokemon2=pokemon6.getName();
+        case 6:
+            nomPokemon2=pokedex.getCatalogo()[5].getName();
             break;
     
         default:
@@ -78,22 +72,22 @@ public class JuegoBase {
     }
     switch (nuPokemon3) {
         case 1:
-            nomPokemon3=pokemon1.getName();
+            nomPokemon3=pokedex.getCatalogo()[0].getName();
             break;
-             case 2:
-            nomPokemon3=pokemon2.getName();
+        case 2:
+            nomPokemon3=pokedex.getCatalogo()[1].getName();
             break;
-             case 3:
-            nomPokemon3=pokemon3.getName();
+        case 3:
+            nomPokemon3=pokedex.getCatalogo()[2].getName();
             break;
-             case 4:
-            nomPokemon3=pokemon4.getName();
+        case 4:
+            nomPokemon3=pokedex.getCatalogo()[3].getName();
             break;
-             case 5:
-            nomPokemon3=pokemon5.getName();
+        case 5:
+            nomPokemon3=pokedex.getCatalogo()[4].getName();
             break;
-             case 6:
-            nomPokemon3=pokemon6.getName();
+        case 6:
+            nomPokemon3=pokedex.getCatalogo()[5].getName();
             break;
     
         default:
---
+++ src/ListaPokemons.java
@@ -0,0 +1,25 @@
+public class ListaPokemons {
+    //la caja donde guardaremos a todos los pokemons
+    Pokemon[] catalogo;
+    public ListaPokemons(){
+        this.catalogo=new Pokemon[6];
+     //crear pokemons
+        Pokemon pokemon1 =new Pokemon("garchomp", "tierra", "dragon", 239);
+        Pokemon pokemon2 =new Pokemon("Lycanroc", "roca","", 198);
+        Pokemon pokemon3 =new Pokemon("Golisopod", "bicho","agua", 181);
+        Pokemon pokemon4 =new Pokemon("Raichu_alola", "electrico","psiquico", 155);
+        Pokemon pokemon5 =new Pokemon("Kommo-o", "dragon","lucha", 181);
+    //guardamos cada pokemon en un hueco del array
+        Pokemon pokemon6 =new Pokemon("Lucario", "lucha","acero", 172);
+            this.catalogo[0]=pokemon1;
+            this.catalogo[1]=pokemon2;
+            this.catalogo[2]=pokemon3;
+            this.catalogo[3]=pokemon4;
+            this.catalogo[4]=pokemon5;
+            this.catalogo[5]=pokemon6;
+    }
+    //getters y setters
+    public Pokemon[] getCatalogo() {
+        return catalogo;
+}
+}
